86dcf31015194fb5a808c90d7bcdc42906aa79ae
jianglei.xu <jianglei.xu@amh-group.com>
2022-11-11 16:14:45 +0800

    上传代码

---
+++ src/main/java/com/mrxu/DumpDataApplication.java
@@ -0,0 +1,13 @@
+package com.mrxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumpDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DumpDataApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mrxu/controller/ConfigController.java
@@ -0,0 +1,4 @@
+package com.mrxu.controller;
+
+public class ConfigController {
+}
